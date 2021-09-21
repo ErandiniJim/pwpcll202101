ca27c78a534e85e456b16d42d5eadaa74127959b
ErandiniJim <erandinijim99@gmail.com>
2021-09-21 14:12:59 -0500

    Agregando nombre compañia

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express', author: 'Erandini', appName: 'webApp' });
+    res.render('index', { title: 'Express', author: 'Erandini', appName: 'webApp', company: 'Awsome Software' });
 });
 
 /*agregando nueva ruta*/
