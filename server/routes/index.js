6036247bb5470625e8efd885646a31cc0c0f88e7
ErandiniJim <erandinijim99@gmail.com>
2021-11-08 23:29:24 -0600

    42 rutas lista ideas

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,10 +2,16 @@
 import homeRouter from './home';
 // Importando router user
 import userRouter from './user';
+// Importante route projects
+import projectRouter from './project';
 
 // Agregando Rutas a app
 const addRoutes = (app) => {
+  // home routes
   app.use('/', homeRouter);
+  // project routes
+  app.use('/projects', projectRouter);
+  // user routes
   app.use('/user', userRouter);
   return app;
 };
