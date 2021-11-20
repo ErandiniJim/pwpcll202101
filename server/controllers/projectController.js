151a6bcfdb7f71de7e54cb640f9f7549bbe06bb9
ErandiniJim <erandinijim99@gmail.com>
2021-11-19 23:56:29 -0600

    44 procesando form

--- server/controllers/projectController.js
+++ server/controllers/projectController.js
@@ -4,13 +4,24 @@ const index = (req, res) => {
   res.send('Respondiendo a "/Projects/index"');
 };
 
-// "/projects/add"
+// GET "/projects/add"
 const add = (req, res) => {
   res.render('project/addView');
+};
+
+// POST "/projects/add"
+const addPost = (req, res) => {
+  // Rescatando la información del formulario
+  const { name, description } = req.body;
+  res.json({
+    name,
+    description,
+  });
 };
 
 // Pendiente por programar
 export default {
   add,
+  addPost,
   index,
 };
--- server/routes/project.js
+++ server/routes/project.js
@@ -1,17 +1,21 @@
 // Importar Router
 import { Router } from 'express';
 
-// TODO: Importar el controlador de proyectos
+//  Importar el controlador de proyectos
 import projectController from '@server/controllers/projectController';
 
 // Creando Instancia de Router
 const router = new Router();
 
-// "/projects" "/projects/index"
+// GET "/projects" "/projects/index"
 router.get(['/', '/index'], projectController.index);
 
-// "/projects/add"
+// GET "/projects/add"
 // sirve el formulario para agregar proyectos
 router.get('/add', projectController.add);
+
+// POST "/projects/add"
+// Procesa el formulario
+router.post('/add', projectController.addPost);
 
 export default router;
